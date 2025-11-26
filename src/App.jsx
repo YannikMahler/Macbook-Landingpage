a69e9948a1abe18d41a22d95022a624f3230fd49
Yannik Mahler <yannik.mahler1@outlook.de>
2025-11-26 18:46:51 +0100

    Implement 3D Product Viewer Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import { Navbar, Hero } from "./components";
+import { ScrollTrigger, SplitText } from "gsap/all";
+import { gsap } from "gsap";
+
+import { Navbar, Hero, ProductViewer } from "./components";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
     return (
     <main>
         <Navbar/>
         <Hero/>
+        <ProductViewer/>
     </main>
 );
 }
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 import Navbar from "./Navbar";
 import Hero from "./Hero";
+import ProductViewer from "./ProductViewer";
 
-export { Navbar, Hero };
+export { Navbar, Hero, ProductViewer };
